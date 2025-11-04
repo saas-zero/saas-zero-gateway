d7a1b1b7d5e2ebdfe0b5b6009ef215ed735d48bc
Kun-GitHub <hot_kun@hotmail.com>
2025-11-04 09:51:23 +0800

    feat(网关服务): 跨服务调用

--- gateway.go
+++ gateway.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/gateway"
 )
 
+var configFile = flag.String("f", "D:\\GolandProjects\\saas-zero\\apps\\saas-zero-gateway\\etc\\gateway.yaml", "the config file")
+
 func main() {
+	flag.Parse()
+
 	var c gateway.GatewayConf
-	conf.MustLoad("etc/gateway.yaml", &c)
+	conf.MustLoad(*configFile, &c)
 
 	gw := gateway.MustNewServer(c)
 	defer gw.Stop()
 
+	fmt.Printf("Starting gateway at %s:%d...\n", c.Host, c.Port)
 	gw.Start()
 }
